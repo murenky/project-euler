55c462fb9e6af5c28cbb3809fec56af3a2749432
murenky <murenky@gmail.com>
2025-01-28 09:00:53 +0300

    Delete Python directory

--- Python/euler1.py
+++
@@ -1,9 +0,0 @@
-# Euler Project проблема 1
-# найти сумму чисел от 1 до 1000, делящихся на 3 или 5
-sum = 0
-for i in range(3,1000,3):
-    sum += i
-for j in range(5,1000,5):
-    if j%3 != 0:
-        sum += j
-print(sum)
--- Python/euler6v2.py
+++
@@ -1,4 +0,0 @@
-sum = 0
-for i in range(1,51):
-    sum += 101*i - i*i
-print(101*101*50*49 + 2*sum)
